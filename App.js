1c587825661fe4597656060afdb62ba3174697ab
jackson <jackson@live.com.sg>
2022-01-15 00:17:31 +0800

    final change

--- App.js
+++ App.js
@@ -1,18 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, Button, ScrollView } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Button, ScrollView, ActivityIndicator } from "react-native";
+import { useEffect } from 'react/cjs/react.development';
 
 
 export default function App() {
-  return (
-    <TouchableOpacity>
-      123
-  </TouchableOpacity>
-  );
+const [loading, setLoading] = useState(true);
+const [arrival, setArrival] = useState("");
+const [busNo, setBusNo] = useState("");
+const [arrivalTime, setArrivalTime] = useState("");
+const onPress = () => loadBusStopData();
+
+
+const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139"
+
+function loadBusStopData() {
+
+  setLoading(true);
+
+fetch(BUSSTOP_URL)
+.then((response) => response.json())
+.then((responseData) => {
+  console.log("Original data:");
+  console.log(responseData);
+  const myBus = responseData.services.filter(
+  (item) => item.no === '155'
+  ) [0];
+  console.log ("My bus:");
+  console.log (myBus.next.time);
+  setArrival(myBus.next.time)
+  setBusNo(myBus.no)
+  setArrivalTime(myBus.next.duration_ms / 60000)
+  setLoading(false);
+
+
+  
+});
 }
 
+useEffect(() => {
+const interval = setInterval(loadBusStopData, 2000);
+return () => clearInterval(interval);
 
+}, []);
+
+return (
+<View style={styles.container}>
+<Text style={styles.bustitle}>{busNo}</Text>
+  <Text style={styles.title}>Bus arrival time:</Text>
+   <Text style={styles.title}>{loading ? <ActivityIndicator size="large" /> : arrival}</Text>
+   <br />
+   <Text style={styles.title}> Bus Arrival in:</Text>
+   <Text style={styles.title}>{arrivalTime} minutes</Text>
+
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+<Text>refresh!</Text>
+  </TouchableOpacity>
+  </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -21,4 +67,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+button: {
+backgroundColor: "green",
+padding: 10,
+margin: 10,
+},
+
+title: {
+fontSize: 20,
+fontWeight: 10000,
+  },
+
+  bustitle: {
+    fontSize: 70,
+    color: "red",
+    fontWeight: 40000,
+      },
+
 });
